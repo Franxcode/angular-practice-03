e9fe805fedf5b36a9bf4755e9f64cd79e4200ad5
Francisco Oliva <francisco.j.oliva@gmail.com>
2021-12-19 22:21:11 -0300

    Angular practice 03.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { Persona } from './persona.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'listado-personas';
+  titulo = 'Listado de Personas';
+  personas: Persona[] = [
+    new Persona('Juan', 'Perez'),
+    new Persona('Laura', 'Juarez'),
+    new Persona('Karla', 'Lara'),
+  ];
+  public nombreInput: string = '';
+  public apellidoInput: string = '';
+
+  agregarPersona(): void {
+    let persona = new Persona(this.nombreInput, this.apellidoInput);
+    this.personas.push(persona);
+    this.nombreInput = '';
+    this.apellidoInput = '';
+  }
 }
---
+++ src/app/persona.model.ts
@@ -0,0 +1,13 @@
+export class Persona {
+  //   public nombre: string = '';
+  //   public apellido: string = '';
+  //   constructor(nombre: string, apellido: string) {
+  //     this.nombre = nombre;
+  //     this.apellido = apellido;
+  //   }
+
+  // Sintaxis simplificada a lo que esta arriba.
+  constructor(public nombre: string, public apellido: string) {
+      
+  }
+}
